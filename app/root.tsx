87c20139a1b2b816f73496c6a9fd85ecedcd0300
daddygi <vlimbaga49@gmail.com>
2026-01-29 06:55:06 +0800

    chore: remove old favicon ico

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,6 @@ import "./app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
-  { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
